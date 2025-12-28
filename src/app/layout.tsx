093d68475a5edab3f566b0fb4a067033618c5e94
Veersen Jadhav <VeersenJadhav@gmail.com>
2025-12-28 14:08:35 +0000

    Updated title text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'Persona Canvas',
+  title: 'Veersen Jadhav: Persona Canvas',
   description: 'My Professional CV and Bio',
 };
 
